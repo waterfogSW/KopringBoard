3ea9c90dfe82fa779a72bf56af21f34f532d39ab
waterfog9580 <waterfog9580@gmail.com>
2022-11-17 00:45:29 +0900

    :tada: project setup

---
+++ src/main/kotlin/com/waterfogsw/kopringboard/Application.kt
@@ -0,0 +1,11 @@
+package com.waterfogsw.kopringboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringBoardApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopringBoardApplication>(*args)
+}
