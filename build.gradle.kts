fa67536d258f33c872acfb7b7bc0a9ef8b771725
waterfog9580 <waterfog9580@gmail.com>
2022-11-17 20:05:33 +0900

    :heavy_plus_sign: 의존성 정리

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,14 +20,27 @@ repositories {
 val snippetsDir by extra { file("build/generated-snippets") }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
+    //web
     implementation("org.springframework.boot:spring-boot-starter-web")
+
+    //jpa
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    //validation
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    //kotlin
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    runtimeOnly("com.h2database:h2")
+
+    //mysql
+    runtimeOnly("mysql:mysql-connector-java")
+
+    //test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    //restDocs
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
 }
 
